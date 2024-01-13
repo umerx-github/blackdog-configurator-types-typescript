d9969cc4965482b026b8c71b2e4e937118c5e328
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-01-13 17:08:31 +0000

    Export all strategy

--- dev/publish.sh
+++ dev/publish.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-git add . && git commit -m"$1" && npm version patch && npm publi
-sh --access public
+git add . && git commit -m"$1" && npm version patch && npm publish --access public
