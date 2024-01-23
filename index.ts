4ba51345b4419d4c8d666055a3d8815869bebe0e
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-01-23 01:09:18 +0000

    Export StrategyTemplateSeaDogDiscountScheme

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * as Strategy from './src/strategy.js';
+export * as StrategyTemplateSeaDogDiscountScheme from './src/strategyTemplateSeaDogDiscountScheme.js';
 export * as StrategyTemplate from './src/strategyTemplate.js';
 export * as Response from './src/response.js';
 export * as Request from './src/request.js';
