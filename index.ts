4b802f5cd550c0391f85f2abde288d601e419fa6
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-01-27 17:09:00 +0000

    Export symbol types

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 export * as Strategy from './src/strategy.js';
 export * as StrategyTemplateSeaDogDiscountScheme from './src/strategyTemplateSeaDogDiscountScheme.js';
+export * as StrategyTemplateSeaDogDiscountSchemeSymbol from './src/strategyTemplateSeaDogDiscountSchemeSymbol.js';
+export * as Symbol from './src/symbol.js';
 export * as StrategyTemplate from './src/strategyTemplate.js';
 export * as Response from './src/response.js';
 export * as Request from './src/request.js';
