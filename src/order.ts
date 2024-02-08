982e2a0fd7f4afa9c4041cf329fa4919115960fa
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-02-08 01:38:22 +0000

    Add Order fill types

--- src/order.ts
+++ src/order.ts
@@ -690,3 +690,33 @@ export function OrderDeleteSingleResponseBodyFromRaw(
 }
 
 // END DELETE
+
+// BEGIN FILL
+
+export interface OrderFillPostSingleRequestParamsRaw {
+    id: string;
+}
+
+export interface OrderFillPostSingleRequestParams {
+    id: number;
+}
+
+export type OrderFillPostSingleRequestBody = never;
+export interface OrderFillPostSingleResponseBody {
+    status: string;
+    message: string;
+    data: OrderResponseBodyDataInstance;
+}
+
+export const OrderFillPostSingleRequestParamsExpected = z.object({
+    id: z.string().regex(/^\d+$/),
+});
+
+export function OrderFillPostSingleRequestParamsFromRaw(
+    raw: OrderFillPostSingleRequestParamsRaw
+): OrderFillPostSingleRequestParams {
+    const parsed = OrderFillPostSingleRequestParamsExpected.parse(raw);
+    return { id: parseInt(parsed.id) };
+}
+
+// END FILL
